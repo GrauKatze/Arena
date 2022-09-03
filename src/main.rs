5e39738cab9da320046757f8e3f74760c40dbb00
GrauKatze <GrauKatze@protonmail.com>
2022-09-03 19:43:20 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use user_mod::user_mod::{hello,initial};
 
 fn main() {
     hello();
-
+    //TODO: add init
     let _user = initial();
 
     println!("User name: {} and health {}", _user.name, _user.health);
